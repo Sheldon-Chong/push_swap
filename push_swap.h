b2dd434e4f3e7c4e140d0b13aeff5652a2c5b749
Sheldon Chong <shechong@u82z01s01.42kl.edu.my>
2023-07-03 17:59:15 +0800

    push test

--- push_swap.h
+++ push_swap.h
@@ -6,13 +6,13 @@
 /*   By: shechong <shechong@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/19 19:08:22 by shechong          #+#    #+#             */
-/*   Updated: 2023/06/23 20:04:04 by shechong         ###   ########.fr       */
+/*   Updated: 2023/07/03 17:56:04 by shechong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
-# include "./libft/libft.h"
+# include "./libft2/libft.h"
 # include <limits.h>
 
 typedef struct s_stack
